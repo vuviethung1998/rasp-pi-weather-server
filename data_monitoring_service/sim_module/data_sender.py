03ddd339b46cbffae683437dce0a847244e02efa
vuviethung1998 <vuviethung.98.hust@gmail.com>
2020-12-25 14:34:36 +0700

    update

--- data_monitoring_service/sim_module/data_sender.py
+++ data_monitoring_service/sim_module/data_sender.py
@@ -4,7 +4,7 @@ import sys
 from sim_module import sim
 import time
 import json
-from sensor_monitoring import dht, wv20,ze12, ze15, ze25, zh03b
+from sensor_monitoring import dht, wv20,ze12, ze15, ze25, zh03b, INA219 as battery
 import board
 
 from time import sleep
@@ -28,6 +28,10 @@ def data_sender(config,debug=True):
     # init params
     URL = getURL(config)
     content_type = config['content_type']
+
+
+    # Init Battery
+    Battery = battery.INA219(addr=0x42)
 
     # init sensor
     # CO2 = wv20.sensor()
@@ -87,14 +91,14 @@ def data_sender(config,debug=True):
             # get data
             pm2_5, ok_pm25 = PM2_5.getSensor()
             temp, humid, ok_dht = DHT.getSensor()
+            voltage, current, power, percent= Battery.getBusVoltage_V(), Battery.getCurrent_mA(), Battery.getPower_W(), Battery.getPercent()
 
             if not ok_pm25:
                 if debug: print('Error read sensor: PM2.5')
             if not ok_dht:
                 if debug: print('Error read sensor: DHT')
 
-
-            data = {'pm2_5_val': pm2_5, 'temp_val': temp, 'humid_val': humid}
+            data = {'pm2_5_val': pm2_5, 'temp_val': temp, 'humid_val': humid, "voltage": voltage, "current": current, "power": power, "battery_percent": percent}
             body_str = get_body_str(data)
             if debug:
                 print('Send data to Server:' + URL)
@@ -103,6 +107,8 @@ def data_sender(config,debug=True):
             if not ok:
                 if debug: print('Error send data to Server')
             sleep(5)
+
+
         except KeyboardInterrupt:
             main_run = False
             sim.gps_stop()
